878cff02881ceb31ad5f3eefb449fb45eaf3c76b
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-11-20 14:10:01 +0530

    Hello World in c++

---
+++ Basics/helloworld.cpp
@@ -0,0 +1,11 @@
+//WAP to print hello world
+#include<iostream>
+using namespace std;
+
+//Namespace = provides a solution for preventing name conflicts in large projects. Each entity needs a unique name. A namespace allow for identically named entities as long as the namespace are different.
+//A namespace is a container that groups related identifiers (like variables, functions, classes, etc.) together to avoid name conflicts.
+//is used to tell the compiler that you want to use all the names (like functions, classes, and objects) that are defined in the std (standard) namespace without writing std:: before them.
+
+int main(){
+    cout<<"Prasasti kumar baitharu";
+}
